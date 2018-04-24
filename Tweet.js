66bcbb983fdfe15324fa60f16d86030c79648383
ZugNZwang <eric.holguinsanchez@gmail.com>
2018-04-24 14:27:24 -0600

    Added Tweet Model

---
+++ Tweet.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Connect to mLab Database
+mongoose.connect(process.env.DB);
+
+// Tweet Schema
+var TweetSchema = new Schema ({
+    twid       : String,
+    active     : Boolean,
+    author     : String,
+    avatar     : String,
+    body       : String,
+    date       : Date,
+    screenName : String
+});
+
+// getTweets method to return tweets from the database
+schema.statics.getTweets = function(page, skip, callback) {
+
+    // Array of tweets
+    var tweets = [];
+
+    var start = (page * 10) + (skip * 1);
+
+    // Query database , using skip and limit to have page chunks
+    Tweet.find({}, 'twid active author avatar body date screenName',
+        {skip: start, limit: 10}).sort({date: 'desc'}).exec(function (err, docs) {
+
+            if(!err) { // If there are no errors
+                tweets = docs; // tweets exist
+                tweets.forEach(function(tweet){
+                    tweet.active = true; // Set each tweet to active
+                })
+            }
+
+            // Pass tweets back to callback
+            callback(tweets);
+    })
+};
+
+// Return a Tweet model based upon the defined schema
+module.exports = Tweet = mongoose.model('Tweet', schema);
---
+++ server.js
@@ -0,0 +1,5 @@
+var express = require('express');
+
+var app = express();
+
+app.listen(process.env.PORT || 8080);
