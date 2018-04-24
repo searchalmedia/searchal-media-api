9aadd0617cd8ff5ea546a73bdad19d95e8c925e7
ZugNZwang <eric.holguinsanchez@gmail.com>
2018-04-24 15:36:39 -0600

    Twitter API

--- server.js
+++ server.js
@@ -1,5 +1,40 @@
+// Require dependencies
 var express = require('express');
+var bodyParser = require('body-parser');
+var Twitter = require('twitter');
+var mongoose = require('mongoose');
+var cors = require('cors');
 
+// Express instance
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Router instance
+var router = express.Router();
+
+// Connect to mLab Database
+mongoose.connect(process.env.DB);
+
+var client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_SECRET
+});
+
+
+router.post('/search', function(req, res){
+    if(!req.body.searchKey) {
+        res.json({success: false, msg: 'Please pass search key.'});
+    }
+    else {
+        var key = req.body.searchKey;
+        client.get('search/tweets', {q: key, result_type: 'popular'}, function(error, tweets, response) {
+            console.log(tweets);
+        });
+    }
+});
+
 
 app.listen(process.env.PORT || 8080);
